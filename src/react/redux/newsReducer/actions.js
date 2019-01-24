c4d7a5007550ee4dc260a5cc77590888aeaca296
Pulkit Aggarwal <pulkit.aggarwal@kelltontech.com>
2019-01-24 13:20:59 +0530

    Add common file for url

--- src/react/redux/newsReducer/actions.js
+++ src/react/redux/newsReducer/actions.js
@@ -1,6 +1,6 @@
 import { FETCH_NEWS_SUCCESS, FETCH_NEWS_FAILURE, HIDE_LOADER, SHOW_LOADER } from './constant';
 import "isomorphic-fetch";
-
+import URLS from "../../common/api";
 
 // Action Creators
 const showLoader = () => ({ type: SHOW_LOADER });
@@ -10,7 +10,7 @@ const newsError = () => ({ type: FETCH_NEWS_FAILURE });
 
 export const fetchNews = () => (dispatch, getState) => {
   dispatch(showLoader());
-  return fetch("http://localhost:3000/api/news")
+  return fetch(URLS.news)
     .then(response => response.json())
     .then(news => {
       dispatch(hideLoader());
