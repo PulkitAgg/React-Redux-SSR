7d5c37a16644883b92e694aba87a835bc2f751c0
Pulkit Aggarwal <pulkit.aggarwal@kelltontech.com>
2019-01-24 17:09:41 +0530

    Resolve confi issues

--- src/react/components/news/newsList.js
+++ src/react/components/news/newsList.js
@@ -2,13 +2,14 @@ import React from "react";
 import "../../assests/css/newsList.css";
 
 export default function NewsList({ news }) {
+  console.log('data',news);
   return (
     <div className="newslist">
       <div className="header">
         <strong>Wizard News</strong>
       </div>
-      {news &&
-        news.map(post =>
+      {news && news.data &&
+        news.data.map(post =>
           <div key={post.id}>
             <p>
               {post.id} ⬆ {post.title}
